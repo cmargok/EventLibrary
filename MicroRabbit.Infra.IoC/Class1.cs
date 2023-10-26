e55de1629118c3ac8fd27ae5ac3a4a72fd09936a
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-25 21:59:51 -0500

    Ioc for rabbit event bus

--- MicroRabbit.Infra.IoC/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MicroRabbit.Infra.IoC
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MicroRabbit.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            services.Configure<RabbitMQSettings>(config.GetSection("RabbitMQSettings"));
+        }
+    }
+}
